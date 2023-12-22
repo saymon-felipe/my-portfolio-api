691116bb15813a8b1bd04c17dd179b1f12771f81
Saymon Felipe <linnubr@gmail.com>
2023-12-21 23:00:23 -0300

    adição do cors e variaveis de ambiente

--- app.js
+++ app.js
@@ -2,22 +2,40 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const rotaContact = require('./routes/contact');
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+const allowedOrigins = [process.env.URL_SITE];
+
+app.use(cors({
+    origin: function(origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not ' +
+                    'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Control-Allow-Header', 
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+        'Access-Control-Allow-Headers',
+        'Content-Type',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
 
-    if (req.method === "OPTIONS") {
-        res.headersSent('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE');
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST, PATCH, DELETE, GET');
         return res.status(200).send({});
     }
 
